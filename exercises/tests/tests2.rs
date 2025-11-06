22707964588df05e7dcfde72569de2db64287dc1
hangyudu <hangyudu@tencent.com>
2025-11-06 15:26:38 +0800

    [main]complete tests.

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -6,12 +6,11 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(0, 0);
+        assert_eq!(0, 0, "left is equal with right!");
     }
 }
