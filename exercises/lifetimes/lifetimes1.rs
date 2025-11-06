b121d07b577e744e07463233951090bf8f1b9a06
hangyudu <hangyudu@tencent.com>
2025-11-06 14:04:22 +0800

    [main]complete lifetimes

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -8,9 +8,8 @@
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-fn longest(x: &str, y: &str) -> &str {
+// "'a" specifies the lifetime of return value is exactly the same as the shorter one of the parameters
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
