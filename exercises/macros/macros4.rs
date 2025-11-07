c3563dde586fdcbb56e27a62b2de0a4c7c8cc068
hangyudu <hangyudu@tencent.com>
2025-11-07 11:46:34 +0800

    [main]complete macros.

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,19 +3,23 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
+    ($val:ident) => {
+        println!("I'm an identifier: value = {}", $val);
+    };
     ($val:expr) => {
-        println!("Look at this other macro: {}", $val);
-    }
+        println!("Look at this other macro: value = {}", $val);
+    };
 }
 
 fn main() {
     my_macro!();
     my_macro!(7777);
+    my_macro!("Generics");
+    let x = 10;
+    my_macro!(x);
 }
