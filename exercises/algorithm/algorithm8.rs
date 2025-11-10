0aa859d33bdcad0ab4a8694f2c19e23a60ee4e20
hangyudu <hangyudu@tencent.com>
2025-11-10 17:53:27 +0800

    [main]complete stack implemented by queue.

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -1,8 +1,7 @@
 /*
 	queue
-	This question requires you to use queues to implement the functionality of the stac
+	This question requires you to use queues to implement the functionality of the stack
 */
-// I AM NOT DONE
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -54,39 +53,85 @@ impl<T> Default for Queue<T> {
 
 pub struct myStack<T>
 {
-	//TODO
-	q1:Queue<T>,
-	q2:Queue<T>
+    q1:Queue<T>,
+    q2:Queue<T>,
+    is_q1: bool,
 }
+
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
-			q1:Queue::<T>::new(),
-			q2:Queue::<T>::new()
+            q1:Queue::<T>::new(),
+            q2:Queue::<T>::new(),
+            is_q1:true
         }
     }
+
     pub fn push(&mut self, elem: T) {
-        //TODO
+        let q = self.cur_queue();
+        q.enqueue(elem);
     }
+
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        if self.is_empty() {
+            return Err("Stack is empty")
+        }
+
+        let mut last = Err("Not Found");
+        while let Ok(first) = self.cur_queue().dequeue() {
+            if !self.is_empty() {
+                self.cur_backup_queue().enqueue(first);
+            } else {
+                last = Ok(first)
+            }
+        }
+
+        self.switch_queue();
+
+        return last;
     }
+
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        self.cur_queue_ro().is_empty()
+    }
+
+    fn switch_queue(&mut self) {
+        self.is_q1 = !self.is_q1;
+    }
+
+    fn cur_queue_ro(&self) -> &Queue<T> {
+        if self.is_q1 {
+            return &self.q1
+        } else {
+            return &self.q2
+        }
+    }
+
+    fn cur_queue(&mut self) -> &mut Queue<T> {
+        if self.is_q1 {
+            return &mut self.q1
+        } else {
+            return &mut self.q2
+        }
+    }
+
+    fn cur_backup_queue(&mut self) -> &mut Queue<T> {
+        if !self.is_q1 {
+            return &mut self.q1
+        } else {
+            return &mut self.q2
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	
-	#[test]
-	fn test_queue(){
-		let mut s = myStack::<i32>::new();
-		assert_eq!(s.pop(), Err("Stack is empty"));
+    use super::*;
+
+    #[test]
+    fn test_queue(){
+        let mut s = myStack::<i32>::new();
+        assert_eq!(s.pop(), Err("Stack is empty"));
         s.push(1);
         s.push(2);
         s.push(3);
@@ -100,5 +145,5 @@ mod tests {
         assert_eq!(s.pop(), Ok(1));
         assert_eq!(s.pop(), Err("Stack is empty"));
         assert_eq!(s.is_empty(), true);
-	}
+    }
 }
